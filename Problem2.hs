d3da2f85cae78a066d897bf0ea27a92d4b1b7358
Michael Slezak <mslezak@imvu.com>
2014-07-24 11:41:09 -0700

    tweak problem 2 answer. make it more readable. add comment describing problem

--- Problem2.hs
+++ Problem2.hs
@@ -1,3 +1,4 @@
+-- find the sum of the even-values fibonacci terms whose values do not exceed four million
 module Problem2 where
 main = print solve
 
@@ -6,5 +7,5 @@ solve = sum $ filter even (fibb [2,1])
 
 fibb :: [Int] -> [Int]
 fibb (a:b:bs)
-    | a < 4000000  = fibb ([a + b] ++ [a] ++ [b] ++ bs)
+    | a < 4000000  = fibb ((a + b): a : b : bs)
     | otherwise = [a] ++ [b] ++ bs
